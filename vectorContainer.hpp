eed653ee3d4ca364da9088f85052ea37641a5b46
nickthechang <nchan080@ucr.edu>
2020-11-18 23:14:29 -0800

    FIXES #1

---
+++ vectorContainer.hpp
@@ -0,0 +1,48 @@
+fndef __VECTORCONTAINER_HPP__
+#define __VECTORCONTAINER_HPP__
+
+#include "container.hpp"
+#include <vector>
+using namespace std;
+
+class Container;
+class Sort;
+
+class VectorContainer : public Container{
+	protected:
+		vector<Base*> baseVector;
+		Sort* sort_function;
+	public:
+		VectorContainer(): sort_function(NULL) {}	
+		VectorContainer(Sort* temp): sort_function(NULL){
+			sort_function = temp;
+		}
+		void sort(){
+		      sort_function->sort(this);
+		}
+		void set_sort_function(Sort* sort_function){
+			this->sort_function = sort_function;
+		}
+		void add_element(Base* element){
+			baseVector.push_back(element);
+		}
+		void print(){
+			for(unsigned int i = 0; i < baseVector.size(); ++i)
+			{
+				cout << baseVector.at(i)-> stringify() << endl;
+			}
+		}
+		void swap(int i,int j){
+			Base* temp2 = baseVector.at(i);
+			baseVector.at(i) = baseVector.at(j);
+			baseVector.at(j) = temp2;
+		}
+		Base* at(int i){
+			return baseVector.at(i);
+		}
+		int size(){
+			return baseVector.size();
+		}
+};
+
+#endif
